66aa1ed0cdee48c4f03a522d3dc607d75709753e
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-06-23 10:16:14 +0530

    modified:   src/components/Card.js 	modified:   src/components/Charts/Bar3D.js 	modified:   src/components/Charts/Column3D.js 	modified:   src/components/Charts/Doughnut2d.js 	modified:   src/components/Charts/ExampleChart.js 	modified:   src/components/Charts/Pie3D.js 	modified:   src/components/Followers.js 	modified:   src/components/Info.js 	modified:   src/components/Navbar.js 	modified:   src/components/Repos.js 	modified:   src/components/Search.js 	modified:   src/components/User.js 	modified:   src/context/context.js 	modified:   src/context/mockData.js/mockUser.js 	modified:   src/pages/Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,21 @@ import React from 'react'
 import { Info, Repos, User, Search, Navbar } from '../components'
 import loadingImage from '../images/preloader.gif'
 import { GithubContext } from '../context/context'
+import { MdAir } from 'react-icons/md'
+
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext)
+
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} alt="loading" className="loading-img" />
+      </main>
+    )
+  }
+
   return (
     <main>
       <Navbar />
